9701a316517e41166ad9229055a5fcb0e23028ee
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2022-02-03 22:32:13 +0900

    タイムゾーン設定(JST)#35

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module TaskManagementSystem
 
     config.i18n.default_locale = :ja
 
+    config.time_zone = 'Tokyo'
+
+    config.active_record.default_timezone = :local
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environmentzs using the files
