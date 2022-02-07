a8d3c82db8e21d976ece4a4dd647366bcf96f8a0
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2022-02-07 19:14:38 +0900

    statusにenum設定#42

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,12 @@ class Task < ApplicationRecord
   validates :content, presence: true
   validates :expiration_deadline, presence: true
 
+  enum status: {
+    not_started_yet: 0,
+    under_start: 1,
+    completion: 2
+  }
+
   scope :recent, -> { order(created_at: 'DESC') }
   scope :near_deadline, -> { order(expiration_deadline: 'ASC') }
 end
