13c56d2f32480e937842277f9fb2675247b3c37d
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2022-02-07 19:02:51 +0900

    taskモデルにstatusカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_105000) do
+ActiveRecord::Schema.define(version: 2022_02_07_100123) do
 
   create_table "tasks", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_02_05_105000) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "expiration_deadline", null: false
+    t.integer "status"
   end
 
 end
