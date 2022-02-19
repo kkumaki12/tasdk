9470982b0143ba1f2d14fa2e636084e48b747556
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2022-02-19 16:51:16 +0900

    rubocop実行

--- db/migrate/20220217141551_add_user_id_to_tasks.rb
+++ db/migrate/20220217141551_add_user_id_to_tasks.rb
@@ -1,6 +1,7 @@
 class AddUserIdToTasks < ActiveRecord::Migration[6.1]
   def change
-    add_column :tasks, :user_id, :bigint, index: true
+    add_column :tasks, :user_id, :bigint
+    add_index :tasks, :user_id
     add_foreign_key :tasks, :users
   end
 end
