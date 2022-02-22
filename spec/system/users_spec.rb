b8f1d3ea4dec51a7be966627d5d468acc18536a2
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2022-02-23 00:48:18 +0900

    system test修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,46 +1,45 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe 'Users', type: :system do
+RSpec.describe "Users", type: :system do
   let(:user) { create(:user) }
-  describe 'ログイン' do
-    let(:login_user) { create(name: 'hoge', email: 'foo@bar.com', password: 'password') }
+  describe "ログイン" do
     before do
       visit login_path
     end
 
-    context '正しい情報が入力されているとき' do
-      it 'ログインできること' do
-        fill_in 'Email', with: 'foo@bar.com'
-        fill_in 'Password', with: 'password'
-        click_button 'ログイン'
-        expect(page).to have_content('ユーザー')
+    context "正しい情報が入力されているとき" do
+      it "ログインできること" do
+        fill_in "Email", with: "#{user.email}"
+        fill_in "Password", with: "#{user.password}"
+        click_button "ログイン"
+        expect(page).to have_content("ユーザー")
       end
     end
 
-    context 'Emailが大文字のとき' do
-      it 'ログインできること' do
-        fill_in 'Email', with: 'FOO@BAR.com'
-        fill_in 'Password', with: 'password'
-        click_button 'ログイン'
-        expect(page).to have_content('ユーザー')
+    context "Emailが大文字のとき" do
+      it "ログインできること" do
+        fill_in "Email", with: "#{user.email.upcase}"
+        fill_in "Password", with: "#{user.password}"
+        click_button "ログイン"
+        expect(page).to have_content("ユーザー")
       end
     end
 
-    context 'Emailが間違っているとき' do
-      it 'ログインできないこと' do
-        fill_in 'Email', with: 'hoge@bar.com'
-        fill_in 'Password', with: 'password'
-        click_button 'ログイン'
-        expect(page).to have_content('メールアドレスかパスワードが間違っています')
+    context "Emailが間違っているとき" do
+      it "ログインできないこと" do
+        fill_in "Email", with: "hoge@bar.com"
+        fill_in "Password", with: "#{user.password}"
+        click_button "ログイン"
+        expect(page).to have_content("メールアドレスかパスワードが間違っています")
       end
     end
 
-    context 'Passwordが間違っているとき' do
-      it 'ログインできないこと' do
-        fill_in 'Email', with: 'foo@bar.com'
-        fill_in 'Password', with: 'hogehoge'
-        click_button 'ログイン'
-        expect(page).to have_content('メールアドレスかパスワードが間違っています')
+    context "Passwordが間違っているとき" do
+      it "ログインできないこと" do
+        fill_in "Email", with: "#{user.email}"
+        fill_in "Password", with: "hogehoge"
+        click_button "ログイン"
+        expect(page).to have_content("メールアドレスかパスワードが間違っています")
       end
     end
   end
