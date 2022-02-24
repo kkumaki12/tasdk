9f9546f585a8aab21b70f5c929f7bd96bab9597a
kkumaki12 <79786417+kkumaki12@users.noreply.github.com>
2022-02-24 22:54:45 +0900

    ログアウト機能実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,11 @@ module SessionsHelper
 
   def current_user
     return unless session[:user_id]
-    User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
